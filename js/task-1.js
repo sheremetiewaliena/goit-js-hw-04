491e30be5d079ce0ec8a6fd81ddd3b0c93242cd6
Елена Шереметьева <142352568+sheremetiewaliena@users.noreply.github.com>
2023-11-18 20:24:49 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+  let toLowerCase = title.toLowerCase()
+  let wordsSplitted = toLowerCase.split(" ")
+  let slug = wordsSplitted.join("-")
+return (slug)  
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+ function filterArray(numbers, value) {
+    const resultArray = new Array();
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            resultArray.push(numbers[i]);
+        }
+    }
+
+    return (resultArray);
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
